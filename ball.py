7827b6d42e4462e157c0dce1b73ccb0c4986e021
Alexander Pashkou <64390864+Kinto711@users.noreply.github.com>
2022-10-14 23:17:19 -0400

    Pong Game

---
+++ ball.py
@@ -0,0 +1,26 @@
+from turtle import Turtle, Screen
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.speed("normal")
+        self.x_move = 1
+        self.y_move = 1
+
+    def move(self):
+        new_x = self.xcor()
+        new_y = self.ycor()
+
+        new_x += self.x_move
+        new_y += self.y_move
+        self.goto(new_x, new_y)
+
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+import time
+
+
 
 turtle = Turtle()
 screen = Screen()
@@ -11,6 +15,7 @@ screen.tracer(0)
 
 
 paddle = Paddle()
+ball = Ball()
 
 screen.listen()
 screen.onkey(paddle.up_left_paddle, "Up")
@@ -21,9 +26,14 @@ screen.onkey(paddle.down_right_paddle, "s")
 game_is_on = True
 while game_is_on:
     screen.update()
-    # for one_paddle in paddle.paddles:
-    #     if one_paddle.ycor() == 600:
-    #
+    ball.move()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(paddle.paddles[0]) < 50 and ball.xcor() > 340:
+        ball.bounce_x()
+    elif ball.distance(paddle.paddles[1]) < 50 and ball.xcor() < -340:
+        ball.bounce_x()
 
 
 
